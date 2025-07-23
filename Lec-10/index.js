8fb6aa2e5942489aa97c205d618cadd34acb833a
Tushar Chaudhary <chaudharytushar270@gmail.com>
2025-07-23 15:45:43 +0530

    10

--- Lec-10/index.js
+++ Lec-10/index.js
@@ -14,13 +14,33 @@ app.use(express.static(__dirname+"/public"))
 //     res.sendFile(__dirname+"/about.html");
 // });
 
-app.post('/submit',(req,res)=>{
-    const {username,password}=req.body;
-    res.json({
-        Username:username,
-        Password:password
+app.post('/submit', (req, res) => {
+    let allUser=[];
+    const { username, password } = req.body;
+
+    const userData = {
+        username,
+        password
+    };
+    fs.readFile("users.json","utf-8",function(err,data){
+        if(err) return res.json({
+            error:err
+        })
+        if(data && data.length>0){
+            allUser=JSON.parse(data);
+        }
+        allUser.push(userData);
+        fs.writeFile("users.json",JSON.stringify(allUser),function(err){
+            if(err) return res.send(err)
+            console.log("Successful");
+        res.json({
+            Username: username,
+            Password: password
+        });
+            
     })
 })
+});
 
 app.listen(port, () => {
     console.log("Server started on port " + port);
