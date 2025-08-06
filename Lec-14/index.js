4fea28e07fad36bf0d8de814c969e9264eb9785c
Tushar Chaudhary <chaudharytushar270@gmail.com>
2025-08-06 14:26:18 +0530

    lec14

---
+++ Lec-14/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const fs=require('fs');
+const PORT = 3000;
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(__dirname+"/public"))
+app.use(express.json())
+app.get('/users', (req, res) => {
+  fs.readFile("./Lec-14/users1.json","utf-8",function(err,data){
+    if(err) res.send(err);
+    let allusers=JSON.parse(data);
+    res.json(allusers);
+  })
+});
+
+app.post("/adduser",(req,res)=>{
+  try {
+    let name=req.body.name;
+    let username=req.body.username;
+    let newUser={
+      id:Math.floor(Math.random()*1000000),
+      name:name,
+      username:username,
+      role:"user"
+  }
+
+  let alluser=[];
+  let data=fs.readFileSync("./Lec-14/users1.json","utf-8");
+  if(data){
+    alluser=JSON.parse(data);
+  }
+  alluser.push(newUser);
+  fs.writeFileSync("./Lec-14/users1.json",JSON.stringify(alluser))
+  res.send("user added")
+
+  } catch (error) {
+    return res.send(error)
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
---
+++ Lec-15/public/index.js
@@ -0,0 +1,65 @@
+const ul = document.querySelector(".user-container");
+let registerform=document.querySelector(".register");
+console.log(registerform)
+let nameInput=document.querySelector(".name");
+let userNameInput=document.querySelector(".username");
+function getUsersData(URL){
+    fetch(URL)
+    .then((res)=>{
+        console.log(res)
+        return res.json()
+    })
+    .then((data)=>{
+        console.log(data)
+        data.forEach(user => {
+            displayUser(user)
+        });
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+}
+
+function displayUser(user){
+    const li = document.createElement("li");
+    li.innerHTML=`
+     <li class="user-item" style="display:flex">
+        <div class="user-info">
+            <h1>${user.name}</h1>
+            <p>${user.username}</p>
+        </div>
+        <div class="user-btn">
+            <button class="user-delete">❤️</button>
+            <button class="user-edit">🤣</button>
+        </div>
+    </li>
+    `
+    ul.appendChild(li);
+}
+getUsersData("http://localhost:3000/users")
+
+function addUser(name,username,URL){
+    let data={
+        name:name,
+        username:username
+    }
+    fetch(URL,{
+        method:"POST",
+        body:JSON.stringify(data),
+        headers:{
+            'content-type':'application/json'
+        }
+    })
+    .then((res)=>{
+        return res.json()
+    })
+    .then((data)=>{
+        console.log(data)
+    })
+}
+registerform.addEventListener("submit",function(e){
+    e.preventDefault();
+    let name=nameInput.value;
+    let username=userNameInput.value;
+    addUser(name,username,"http://localhost:3000/adduser")
+})
