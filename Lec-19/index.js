f842d45e9d65be9b9962fa477c736b8f5985f998
Tushar Chaudhary <chaudharytushar270@gmail.com>
2025-08-18 15:27:15 +0530

    paratham

--- Lec-19/index.js
+++ Lec-19/index.js
@@ -12,42 +12,10 @@ const user = require('./model/user');
 
 let blogRoute = require("./routes/blogRoutes");
 let userRoute=require("./routes/usersRoutes");
+
+
 app.use("/api/users",userRoute)
 app.use("/api/blogs",blogRoute)
-
-app.post("/users",async(req,res)=>{
-    let {email,username,password} = req.body;
-    let newUser = new Users({
-        email:email,
-        username:username,
-        password:password
-    })
-    await newUser.save()
-    res.json({
-        success: true,
-        data: newUser,
-        message: "User created successfully"
-    });
-})
-
-app.get("/users",async(req,res)=>{
-    let allUsers = await Users.find();
-    res.json({
-        success: true,
-        data: allUsers
-    });
-})
-
-app.get("/users/:id",async(req,res)=>{
-    let {id} = req.params;
-    let userExist = await Users.findOne({_id: id}).populate("blogs");
-    if(userExist){
-        res.json({
-        success: true,
-        data: userExist
-    });
-    }
-})
 
 
 
--- Lec-19/routes/usersRoutes.js
+++ Lec-19/routes/usersRoutes.js
@@ -1,38 +1,14 @@
-const express=require("express");
-const router=express.Router();
+const express = require("express");
+const router = express.Router();
 
-app.post("/",async(req,res)=>{
-    let {email,username,password} = req.body;
-    let newUser = new Users({
-        email:email,
-        username:username,
-        password:password
-    })
-    await newUser.save()
-    res.json({
-        success: true,
-        data: newUser,
-        message: "User created successfully"
-    });
-})
+const {
+    createUser,
+    getAllUsers,
+    getOneUser
+} = require("../controller/usersController");
 
-app.get("/",async(req,res)=>{
-    let allUsers = await Users.find();
-    res.json({
-        success: true,
-        data: allUsers
-    });
-})
+router.post("/", createUser);
+router.get("/", getAllUsers);
+router.get("/:id", getOneUser);
 
-app.get("/:id",async(req,res)=>{
-    let {id} = req.params;
-    let userExist = await Users.findOne({_id: id}).populate("blogs");
-    if(userExist){
-        res.json({
-        success: true,
-        data: userExist
-    });
-    }
-})
-module.exports=router
-
+module.exports = router;
