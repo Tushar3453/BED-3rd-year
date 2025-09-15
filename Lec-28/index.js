40fc381e94d85a85efdc46b4dc44eea5e87b06bc
Tushar Chaudhary <chaudharytushar270@gmail.com>
2025-09-15 15:43:28 +0530

    lec-28

---
+++ Lec-28/index.js
@@ -0,0 +1,25 @@
+const {PrismaClient} = require("./generated/prisma");
+const prisma = new PrismaClient();
+async function addUser(email,name,password){
+    await prisma.user.create({
+        data:{
+            email:email,
+            name:name,
+            password:password
+        }
+    })
+}
+// addUser("chaudharytushar270@gmail.com","Tushar","1234")
+// .then(()=>{
+//     console.log("user added successfully!!")
+// })
+
+async function getAllUser(){
+    let allUser=await prisma.user.findMany();
+
+    return allUser;
+}
+getAllUser()
+.then((data)=>{
+    console.log(data);
+})
