f312edeaab456e381f3099f715ba94225b594983
Tushar Chaudhary <chaudharytushar270@gmail.com>
2025-11-10 15:01:14 +0530

    lec-41 new

--- Lec-41/api/controller/order.js
+++ Lec-41/api/controller/order.js
@@ -5,5 +5,6 @@ module.exports.postPlaceOrder=async (req,res)=>{
     let{side,type,price,quantity,user}=req.body;
     let response=ob.placeOrder(side,type,price,quantity,user);
     console.log(response)
+    res.send(response);
 
 }
