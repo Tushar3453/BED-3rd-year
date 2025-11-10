cd432c269d85a7c8f8e654c38bf18c027a368ecc
Tushar Chaudhary <chaudharytushar270@gmail.com>
2025-11-10 14:44:54 +0530

    lec-41

---
+++ Lec-41/controller/order.js
@@ -0,0 +1,9 @@
+const OrderBook = require("../service/order");
+let ob=new OrderBook("BTCUSD"); //global object
+
+module.exports.postPlaceOrder=async (req,res)=>{
+    let{side,type,price,quantity,user}=req.body;
+    let response=ob.placeOrder(side,type,price,quantity,user);
+    console.log(response)
+
+}
